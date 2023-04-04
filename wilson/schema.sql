844f56ad3c1cfa6e588a4df9c62947b7582f57f4
MovieGoers <alscjf4104@gmail.com>
2023-04-04 18:47:22 +0900

    changed

--- wilson/schema.sql
+++ wilson/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
-  nickname TEXT UNIQUE NOT NULL
+  nickname TEXT NOT NULL
 );
 
 CREATE TABLE convo (
