666aa56af4c086f65c0b72e4acede66161ebf5b8
MovieGoers <alscjf4104@gmail.com>
2023-04-07 17:11:00 +0900

    deleted talkingtowilson

--- wilson/mainpage.py
+++ wilson/mainpage.py
@@ -29,12 +29,14 @@ bp = Blueprint('mainpage', __name__)
 def mainpage():
     user_id = session.get('user_id')
     
-    conversation = []
+    # conversation = []
 
-    conversation.append({'role': 'system', 'content': 'say Cheese!'}) #Say Cheese GPT~~
-    conversation = ChatGPT_conversation(conversation)
+    # conversation.append({'role': 'system', 'content': 'say Cheese!'}) #Say Cheese GPT~~
+    # conversation = ChatGPT_conversation(conversation)
 
-    GPT_today = conversation[-1]['content'].strip()
+    # GPT_today = conversation[-1]['content'].strip()
+    GPT_today = "Hello~"
+
     nickname = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
@@ -44,45 +46,8 @@ def mainpage():
 @bp.route('/talktowilson', methods=['GET', 'POST'])
 @login_required
 def talktowilson():
-    #사용자가 버튼을 클릭시 request.form으로 data가 talkingtowilson으로 전송된다.
-    if request.method == 'POST':
-        return redirect(url_for("talkingtowilson"))
-    else:
-         return render_template('mainpage/talktowilson.html')
 
-@bp.route('/talkingtowilson', methods=['GET', 'POST'])
-@login_required
-def talkingtowilson():
-    #talktowilson으로부터 전달받은 data 사용.
-    conversation = []
-
-    user_topic = request.form.get('input_interest')
-
-    first_input = 'Lets do a role play that can happen in real life about '
-    first_input += user_topic
-    first_input += '. what scenario are we going to play?'
-    conversation.append({'role': 'system', 'content': first_input})
-    conversation = ChatGPT_conversation(conversation)
-    background_desc = conversation[-1]['content'].strip()
-
-    second_input = 'what is my role? answer it in a single word.'
-    conversation.append({'role': 'user', 'content': second_input})
-    conversation = ChatGPT_conversation(conversation)
-
-    user_role = conversation[-1]['content'].strip()
-
-    final_input = 'What is your role? answer it in a single word.'
-    conversation.append({'role': 'user', 'content': final_input})
-    conversation = ChatGPT_conversation(conversation)
-
-    wilson_role = conversation[-1]['content'].strip()
-
-    conversation.append({'role': 'user', 'content': 'So, You are '+wilson_role+', and I am '+user_role+'. Ok, Lets start now. Hello ' + wilson_role})
-    conversation = ChatGPT_conversation(conversation)
-
-    wilson_speaking = conversation[-1]['content'].strip()
-
-    return render_template('mainpage/talkingtowilson.html', wilson_role = wilson_role, user_role = user_role, background_desc = background_desc, wilson_speaking = wilson_speaking)
+    return render_template('mainpage/talktowilson.html')
 
 @bp.route('/testpage', methods=['GET', 'POST'])
 def testpage():
